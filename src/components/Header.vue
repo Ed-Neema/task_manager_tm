b3b4b756481b1ebfbd89859029db0dfc6349cead
Ed-Neema <ednahakoth21@gmail.com>
2022-06-25 21:18:56 +0200

    Button component made general

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
         <h1>
             {{title}}
         </h1>
-        <ButtonComponent @toggle-add-task="$emit('toggle-add-task')"  :btnTitle="showAddTask ? 'Close': 'Add Task'" :color="showAddTask ? 'red': 'green'" />
+        <ButtonComponent @btn-click="$emit('toggle-add-task')"  :btnTitle="showAddTask ? 'Close': 'Add Task'" :color="showAddTask ? 'red': 'green'" />
     </header>
 </template>
 
